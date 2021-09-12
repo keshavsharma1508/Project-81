7601978ac580f593f5afc365310781e4007ac6c0
keshavsharma1508 <85341193+keshavsharma1508@users.noreply.github.com>
2021-09-12 10:53:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,29 +11,29 @@ ctx.stroke();
 ctx.beginPath();
 ctx.strokeStyle = "blue";
 ctx.lineWidth = 5;
-ctx.rect(250 , 210 , 40 , 0 , 2*Math.PI);
+ctx.arc(250 , 210 , 40 , 0 , 2*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "black";
 ctx.lineWidth = 5;
-ctx.rect(350 , 210 , 40 , 0 , 2*Math.PI);
+ctx.arc(350 , 210 , 40 , 0 , 2*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "red";
 ctx.lineWidth = 5;
-ctx.rect(400 , 210 , 40 , 0 , 2*Math.PI);
+ctx.arc(450 , 210 , 40 , 0 , 2*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "orange";
 ctx.lineWidth = 5;
-ctx.rect(300 , 250 , 40 , 0 , 2*Math.PI);
+ctx.arc(300 , 250 , 40 , 0 , 2*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "green";
 ctx.lineWidth = 5;
-ctx.rect(400 , 250 , 40 , 0 , 2*Math.PI);
+ctx.arc(400 , 250 , 40 , 0 , 2*Math.PI);
 ctx.stroke();
